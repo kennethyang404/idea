08ae7ede5cfdf44ac31859c0020cf1fbbe1ab409
GRMmouse <ail1@andrew.cmu.edu>
2015-02-06 21:55:58 -0500

    posts class and db

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/posts.db"
+
+db = SQLAlchemy(app)
+
+def compare(left,right):
+    return cmp(-left.score,-right.score)
+
+class posts(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    subtitle=db.Column(db.String(140))
+    text = db.Column(db.String(1000))
+    score = db.Column(db.Integer)
+    date = db.Column(db.DateTime())
+    
+    def __init__(self, title, subtitle, text):
+        self.title = title
+        self.subtitle=subtitle
+        self.text = text
+        self.score=score
+        self.date = datetime.utcnow()
+
+db.create_all()
 
 @app.route('/')
 def login():
@@ -8,7 +37,8 @@ def login():
 
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    posts=sorted(db.query.all(),compare)[0:9]
+    return render_template("index.html",posts=posts)
 
 @app.route('/create')
 def create():
