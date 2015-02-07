54b15a29500d533ee61852fb5c3c5a78326c9785
cyang <canyang@andrew.cmu.edu>
2015-02-07 04:28:16 -0500

    fix an important bug

--- main.py
+++ main.py
@@ -6,40 +6,32 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/posts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/projects.db"
 
 db = SQLAlchemy(app)
 
-class posts(db.Model):
+class projects(db.Model):
     ID = db.Column(db.Integer, primary_key=True)
-    owner=db.column(db.String(100))
+    ownerr=db.Column(db.String(100))
     title = db.Column(db.String(100))
     keywords=db.Column(db.String(100))
     objective = db.Column(db.String(1000))
     description=db.Column(db.String(1000))
+    requirement=db.Column(db.String(1000))
     announcement=db.Column(db.String(1000))
     
-    def __init__(self, owner, title, keywords,objective,description,announcement):
-        self.owner=owner
+    def __init__(self, ownerr, title, keywords,objective,description,requirement,announcement):
+        self.ownerr=ownerr
         self.title=title
         self.keywords=keywords
         self.objective=objective
         self.description=description
+        self.requirement=requirement
         self.announcement=announcement
 
 db.create_all()
 
 #Helper Function Used by GRMmouse
-def compare(left,right):
-    return cmp(-left.score,-right.score)
-
-def ownerHandler(owner):
-    owner=owner[3:]
-    if owner.split("%40")[1:]=="andrew.cmu.edu":
-        owner=owner.split("%40")[0]
-        return owner
-    else:
-        return None
 
 def keywordsHandler(keywords):
     keywords=keywords[3:]
@@ -52,17 +44,17 @@ def keywordsHandler(keywords):
 
 def contentHandler(content):
     "Takes the string after post/ and returns a posts object"
-    content=(content[1:]).split("&")
-    owner=ownerHandler(content[0])
-    title=content[1][3:]
-    keywords=keywordsHandler(content[2])
-    objective=content[3][3:]
-    description=content[4][3:]
-    announcement=content[5][3:]
-    if owner!=None and keywords!=None:
-        return posts(owner,title,keywords,objective,description,announcement)
-    else:
-        return None
+    owner=content["q1"]
+    title=content["q2"]
+    keywords=keywordsHandler(content["q3"])
+    objective=content["q4"]
+    description=content["q5"]
+    requirement=content["q6"]
+    announcement=content["q7"]
+    #if keywords!=None:
+    return projects(owner,title,content["q3"],objective,description,requirement,announcement)
+    #else:
+    #    return None
 
 
 @app.route('/')
@@ -71,18 +63,17 @@ def login():
 
 @app.route('/index')
 def index():
-    testttt="<p>Climb leg make muffins or sweet"
-    test=[testttt]*4+(posts.query.all()[:1])*5
+    test=(projects.query.all()[:4])*3
     return render_template("index.html", posts=test)
 
 @app.route('/create')
 def create():
     return render_template("create.html")
 
-@app.route('/newpost/<string:content>') 
-def newpost(content):
-    new=contentHandler(content)
-    db.session.add(new)
+@app.route('/newpost', methods=["POST"]) 
+def newpost():
+    newProject=contentHandler(request.form)
+    db.session.add(newProject)
     db.session.commit()
     return redirect(url_for("index"))
 
